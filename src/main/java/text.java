064c7ae9bd61588dc6dcdd359bcbb1278a5fcb1e
神明佑我 <154118848+AAZl3l4@users.noreply.github.com>
2024-06-03 12:52:27 +0800

    Add files via upload
    于六月二日完成

---
+++ src/main/java/text.java
@@ -0,0 +1,8 @@
+public class text {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            final int part = i;
+            System.out.println(part);
+        }
+    }
+}
