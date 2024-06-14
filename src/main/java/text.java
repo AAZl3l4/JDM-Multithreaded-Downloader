dd4e17eeb98f7ba030666e1bbaa28fcb82749a6a
神明佑我 <154118848+AAZl3l4@users.noreply.github.com>
2024-06-14 10:16:05 +0800

    Delete src directory

--- src/main/java/text.java
+++
@@ -1,8 +0,0 @@
-public class text {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            final int part = i;
-            System.out.println(part);
-        }
-    }
-}
